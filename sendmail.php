3dfcd05be5067e257e42d57f1190edd2a61f84c7
Jorge <jorgeboisson99@gmail.com>
2021-12-02 21:26:17 -0800

    se agrega el proyecto

---
+++ sendmail.php
@@ -0,0 +1,18 @@
+<?php
+    
+    $email_to =   'monnaf37@gmail.com.com'; 
+    $name     =   $_POST['name'];  
+    $email    =   $_POST['email'];
+    $subject  =   $_POST['subject'];
+    $message  =   $_POST['message'];
+    
+    
+    $headers  = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    
+    if(mail($email_to, $subject, $message, $headers)){
+        echo 'sent';     
+    }else{
+        echo 'failed'; 
+    }
+?>
